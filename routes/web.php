7f8e8a8e0853dc07e6368fada8c18ad94f98ad8f
ibnucoding2423 <oniichanterritory@gmail.com>
2025-03-23 15:03:22 +0800

    temporary: deactivating auth feature

--- routes/web.php
+++ routes/web.php
@@ -7,17 +7,17 @@ Route::get('/', function () {
     return Inertia::render('welcome');
 })->name('home');
 
-Route::middleware(['auth', 'verified'])->group(function () {
+// Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
         return Inertia::render('dashboard');
     })->name('dashboard');
-});
+// });
 
-Route::middleware(['auth', 'verified'])->group(function () {
+// Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('admin', function () {
         return Inertia::render('admin');
     })->name('admin');
-});
+// });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+// require __DIR__.'/settings.php';
+// require __DIR__.'/auth.php';
