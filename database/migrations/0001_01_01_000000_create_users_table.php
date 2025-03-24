d2ced0082a2b8965645f9d6435edd6f6dccec0cb
sleepy <nemorzgm.05@gmail.com>
2025-03-24 21:00:15 +0800

    make seeder

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\Hash;
+
 
 return new class extends Migration
 {
@@ -14,7 +16,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('username')->unique();
-            $table->string('password');
+            $table->string('password')->default(Hash::make('password'));
             $table->string('usertype')->default('admin');
             $table->timestamps();
         });
