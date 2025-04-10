13b5a400ed5a6b5cfb99375cf84d8ee4009944c9
sleepy <nemorzgm.05@gmail.com>
2025-04-10 14:50:09 +0800

    Carbrand Controller Model and migration(Untested)

---
+++ app/Http/Requests/Product/CarBrandRequest.php
@@ -0,0 +1,55 @@
+<?php
+namespace App\Http\Requests\Product;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class CarRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'model' => ['required', 'string', 'max:255'],
+            'brand_id' => ['required', 'exists:brands,id'],
+            'car_image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+        ];
+    }
+}
+
+
+class BrandRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255', Rule::unique('brands')->ignore($this->brand)],
+            'brand_logo' => ['required', 'image', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
+        ];
+    }
+}
