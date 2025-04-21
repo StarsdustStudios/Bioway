57a5ead576865a21486a2254e78b9a362d39f46b
ibnucoding2423 <oniichanterritory@gmail.com>
2025-04-21 11:13:03 +0800

    add: mitra/partner

--- resources/js/pages/Admin/option/brand/BrandPage.tsx
+++ resources/js/pages/Admin/option/brand/BrandPage.tsx
@@ -14,9 +14,9 @@ import ItemDataProvider, { useItemData } from '@/context/item-data-context'
 import { itemDatas } from '@/components/data/item-data'
 import { IconEdit, IconTrash, IconUserPlus } from '@tabler/icons-react'
 import { Cross2Icon, DotsHorizontalIcon } from '@radix-ui/react-icons'
-import { BrandGetData, brandGetSchema } from '../sponsor/components/schema'
-import { ItemDataActionDialog } from '../sponsor/components/add-item-data-dialog'
-import { UsersDeleteDialog } from '../sponsor/components/delete-item-data-dialog'
+import { BrandGetData, brandGetSchema } from '../brand/components/schema'
+import { ItemDataActionDialog } from '../brand/components/add-item-data-dialog'
+import { UsersDeleteDialog } from '../brand/components/delete-item-data-dialog'
 import {
   ColumnDef,
   Row,
