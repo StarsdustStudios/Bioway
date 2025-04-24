a6c3d2787485cf6e078eeb958c2ca0c61181df05
sleepy <nemorzgm.05@gmail.com>
2025-04-24 22:17:20 +0800

    update rental and make category and post

--- resources/js/components/global/RentalCard.tsx
+++ resources/js/components/global/RentalCard.tsx
@@ -6,15 +6,15 @@ import { IconBriefcase2, IconArmchair } from "@tabler/icons-react";
 
 export function RentalCard() {
     const carName = faker.vehicle.vehicle();
-    const carImage = faker.image.urlPicsumPhotos({ width: 400, height: 240 });
+    const carImage = faker.image.urlPicsumPhotos({ width: 1920, height: 1080 });
     const seatCount = faker.number.int({ min: 2, max: 7 });
     const luggageCount = faker.number.int({ min: 1, max: 4 });
     const price = faker.number.int({ min: 300000, max: 2000000 });
 
     return (
         <Card className="flex p-4 gap-4 rounded-2xl shadow-md border-blue-300">
-            <div className="flex items-center gap-4">
-                <div className="w-32 h-20 md:w-40 md:h-24 relative">
+            <div className="flex items-center gap-4 py-0">
+                <div className="aspect-video max-w-56">
                     <img
                         src={carImage}
                         alt={carName}
@@ -22,8 +22,8 @@ export function RentalCard() {
                     />
                 </div>
                 <div className="flex flex-col gap-2 flex-1">
-                    <h2 className="text-lg font-semibold">{carName}</h2>
-                    <div className="flex items-center gap-4 text-sm text-muted-foreground">
+                    <h2 className="md:text-md text-sm font-semibold">{carName}</h2>
+                    <div className="flex items-center gap-4 text-xs font-bold">
                         <span className="flex items-center gap-1">
                             <IconArmchair size={18} /> {seatCount}
                         </span>
@@ -31,10 +31,10 @@ export function RentalCard() {
                             <IconBriefcase2 size={18} /> {luggageCount}
                         </span>
                     </div>
-                    <p className="text-xl font-bold text-black">
+                    <p className="md:text-lg text-base font-bold text-black">
                         Rp {price.toLocaleString('id-ID')} / Hari
                     </p>
-                    <Button className="w-fit mt-2 bg-blue-600 hover:bg-blue-700 text-white">
+                    <Button className="w-fit mt-2 bg-blue-600 hover:bg-blue-700 text-white md:text-base text-xs">
                         ORDER SEKARANG
                     </Button>
                 </div>
