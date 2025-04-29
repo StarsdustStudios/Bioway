9af51f74200fbe830b138e0c6dc0128750de62cd
sleepy <nemorzgm.05@gmail.com>
2025-04-29 19:42:06 +0800

    update tour loc migration

--- database/migrations/2025_04_29_114102_add_id_column_in_tourloc.php
+++ database/migrations/2025_04_29_114102_add_id_column_in_tourloc.php
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('tourloc', function (Blueprint $table) {
+        Schema::table('tour_location', function (Blueprint $table) {
             //
         });
     }
