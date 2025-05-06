cd0cf8bbd1a101fa6e5d709c5495238ca2d27859
sleepy <nemorzgm.05@gmail.com>
2025-05-06 23:06:53 +0800

    fix: change content column type to longtext in posts migration and clean up Blog component

--- resources/js/pages/Main/Blog/Blog.tsx
+++ resources/js/pages/Main/Blog/Blog.tsx
@@ -1,19 +1,10 @@
 import MainLayout from '../Main'
-import { PromoCarousel } from '@/components/global/PromoCarousel'
-import { PartnerCarousel } from '@/components/global/PartnerCarousel'
-import { ProductNav } from '@/components/global/ProductNav'
-import { Button } from '@/components/ui/button'
-// import { PartnerCarousel } from '@/components/global/PartnerCarousel'
 
 function Blog() {
     return (
         <>
 
             <h1 className='text-4xl font-bold my-5'>Blog</h1>
-
-            <div>
-                <h1 className='text-4xl font-bold my-15'>Artikel Terbaru</h1>
-            </div>
 
             <div className='md:mb-0 mb-20'>
             </div>
