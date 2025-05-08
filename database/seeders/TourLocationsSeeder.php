425f76d83e789cf127f7b0425b7d89574ece4511
ibnucoding2423 <oniichanterritory@gmail.com>
2025-05-08 22:07:56 +0800

    fix: some seeder

--- database/seeders/TourLocationsSeeder.php
+++ database/seeders/TourLocationsSeeder.php
@@ -5,16 +5,19 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use Illuminate\Support\Facades\DB;
+
 class TourLocationsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        TourLocation::factory()->count(70)->create([
-            'tour_id' => rand(1, 50),
-            'location_id' => rand(1, 10),
-        ]);
+        for ($i = 0; $i < 70; $i++) {
+            DB::table('tour_locations')->insert([
+                'tour_id' => rand(1, 50),
+                'location_id' => rand(1, 10),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
